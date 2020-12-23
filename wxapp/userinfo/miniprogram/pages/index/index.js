88feea775f32f28882c358bce42ea65b9ac0f39d
wujueying <652178160@qq.com>
2020-12-23 21:19:48 +0800

    git command

---
+++ wxapp/userinfo/miniprogram/pages/index/index.js
@@ -0,0 +1,51 @@
+// 云数据库
+const db = wx.cloud.database()
+// userInfo表
+const userInfo = db.collection('userInfo')
+Page({
+  data:{
+    userList:[]
+  },
+  navigatTo(){
+
+  },
+  getUserInfo(result){
+    // console.log(result.detail.userInfo);
+    // 一次上云事件
+    wx.cloud.callFunction({
+      name: 'getOpenId',
+      complete: res => {
+        console.log(res)
+        // 在哪里  mysql where 条件查询  
+        userInfo.where({
+          // 去云端就是为了拿openId 
+          // 用户的openId是唯一的, 
+          _openid:res.result.openId
+        }).count().then( res => {
+          console.log(res);
+          // 新用户
+          if (res.total == 0){
+            // 插入用户
+            // Insert  插入
+            userInfo.add({
+              data: result.detail.userInfo
+            }).then(res => {
+              wx.navigateTo({
+                url: '../add/add',
+              })
+            }).catch(err => {
+              console.error(err)
+            })
+          }else{
+            wx.navigateTo({
+            url: '../add/add',
+          })
+        }
+        })
+      }
+    })
+  }
+   
+
+
+})
