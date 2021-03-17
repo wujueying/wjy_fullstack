291a614f8a6f9dadddd579e76888e555a7b5f22d
wujueying <73487857+wujueying@users.noreply.github.com>
2021-03-17 23:44:34 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter } from "react-router-dom"
+// SPA  #/ 兼容性 / 后端
+// src/ 目录 架构意义
+// /components /pages /api /store /layout页面版式
+import Frame from './layout/Frame';  // 
+import { Switch ,Route} from 'react-router-dom';
+import HookMovie from './pages/HookMovie';
+import HookInfos from './pages/HookInfos';
+
+
+function App() {
+  return (
+   <BrowserRouter>
+   <Frame>
+     <Switch>
+       <Route exact path="/" component={HookMovie}/>
+       <Route path="/infos" component={HookInfos}/>
+     </Switch>
+   </Frame>
+   </BrowserRouter>
+  );
+}
+
+export default App;
