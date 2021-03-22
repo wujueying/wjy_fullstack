40daca44192d23f3b7a6b2c38c693e5da3a5955b
wujueying <73487857+wujueying@users.noreply.github.com>
2021-03-22 18:06:27 +0800

    Add files via upload

---
+++ ReactHooksDemo/demo01/src/Example.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect} from 'react';
+function Example(){  //相当于组件
+    // 第一个是值，第二个是函数，用来改变前者的值
+    const [ count, setCount] = useState(0)  // 初始值设定为0 useState是一个方法 数组解构
+    useEffect( ()=>{
+        console.log(`useEffect=>You clicked ${count} times`)
+    })
+    // let _useState = userState0(0)  // 局部变量 相当于一个函数
+    // let count = _useState[0]  // 数组的第一位
+    // let setState = _useState[1]  // 
+    return (
+        <div>
+            <p>You clicked {count} times</p>  
+            <button onClick={()=>{setCount(count+1)}}>Clicked me</button>
+        </div>
+    )
+}
+
+export default Example;
