ddb3d49ad7813531010ff402087d3fe319402676
wujueying <652178160@qq.com>
2021-03-26 20:47:57 +0800

    XXX

---
+++ components/NavRight/index.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { Tag } from 'antd';
+import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+import "./mock.js";
+import './index.css';
+
+const NavRight = () => {
+  const [tagList, setTagList] = useState([])
+  const getTagAPIList = () =>{
+    axios
+      .get('/tags')
+      .then(res => {
+        console.log(res);
+      })
+  }
+  useEffect(() => {
+    getTagAPIList()
+  }, [])
+  return (
+    <>
+    </>
+  );
+}
+
+export default NavRight;
