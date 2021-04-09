84f0c1e8ded52a5f0e4a8d5217878d70d0332db2
wujueying <652178160@qq.com>
2021-04-09 21:29:46 +0800

    git command

--- interview/zj/test.js
+++ interview/zj/test.js
@@ -1,26 +1,34 @@
-function getConstant(){
+function getConstant() {
     return 1
-}
-
-async function getAsyncConstant(){
+  }
+  
+  function getAsyncConstant() {
     return Promise.resolve().then(() => {
-        return 1
+      return 1
     })
-}
-
-async function getPromise(){
-    return new Promise((resolved, rejected) => {
+  }
+  
+  function getPromise() {
+    return Promise.resolve().then(() => {
+      return new Promise((resolved, rejected) => {
         resolved(1)
+      })
     })
-}
-
-async function test(){
-    let a = 2
-    let c = 1
-    await getConstant()
-    let d = 3
-    await getPromise()
-    let e = 4
-    await getAsyncConstant()
-    return 2
-}
+  }
+  
+  function test() {
+    return Promise.resolve()
+    .then(() => {
+      let a = 2
+      let c = 1
+      return getConstant()
+    }).then(() => {
+      let d = 3
+      return getPromise()
+    }).then(() => {
+      let e = 4
+      return getAsyncConstant()
+    }).then(() => {
+      return 2
+    })
+  }
