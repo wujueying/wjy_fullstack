a068f745880ded1f6aaa1bf07f0bbe46c05f0f8c
wujueying <652178160@qq.com>
2021-04-10 17:37:36 +0800

    提交说明

--- vue/shop/src/router/index.js
+++ vue/shop/src/router/index.js
@@ -2,7 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 
 const routes = [
- 
+ {
+   path:'/login',
+   name: 'login',
+   component: () => import('@/views/Login.vue')
+ }
 ]
 
 const router = createRouter({
