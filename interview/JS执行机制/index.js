39993c7823e272ce868470e7141ed059bdcf8750
wujueying <652178160@qq.com>
2021-04-24 22:06:38 +0800

    git command

---
+++ interview/JS执行机制/index.js
@@ -0,0 +1,37 @@
+// showName()
+// console.log(myname);
+// var myname = 'yy'
+// function showName() {
+//     console.log('函数showname被执行');
+// }
+
+// // myname: undefined
+// // showName: function(){}
+
+
+// myname = 'www'
+
+
+// var a = 2
+// function add(b,c){
+//   return b+c
+// }
+// function addAll(b,c){
+// var d = 10
+// result = add(b,c)
+// return  a+result+d
+// }
+// addAll(3,6)
+
+
+function bar(){
+    console.log(myName);
+}
+
+function foo(){
+    var myName = '夹心'
+    bar()
+}
+
+var myName = 'wjx'
+foo()
