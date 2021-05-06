1a677679eb743b6e7bd88f636bba7c27de4e09d2
wujueying <652178160@qq.com>
2021-05-06 17:53:43 +0800

    git command

---
+++ interview/react2/life-cycle/src/LifeCycle.js
@@ -0,0 +1,69 @@
+import React,{Component} from 'react';
+
+export default class LifeCycle extends Component {
+    static defaultProps = {
+        name: '计数器'
+    }
+    constructor(props){
+        super(props);
+        this.state = {
+            number: 0,
+            user: []
+        }
+          console.log('1. constructor 初始化 props and state')
+    }
+    componentWillUnmount(){
+        console.log('2. componentWillMount 组件将要挂载')
+    }
+    componentDidMount(){
+        console.log('4. componentDidMount 组件已挂载')
+        // fetch 
+    }
+    add = () => {
+        this.setState({ number: this.state.number });
+    }
+    render(){
+        console.log('3. render渲染， 也就是挂载')
+        return(
+            <div>
+                <p>{this.props.name}:{this.state.number}</p>
+                <button onClick={this.add}>+</button>
+                {this.state.number % 2 ===0 && <SubCounter number={this.state.number}/>}
+            </div>
+        )
+    }
+}
+
+class SubCounter extends Component {
+    constructor(props){
+        super(props);
+    }
+    componentWillReceiveProps(){
+        console.log('SubCounter 1.commponentWillReceiveProps');
+    }
+    shouldComponentUpdate(nextProps,nextState){
+        console.log('SubCounter',nextProps,nextState)
+        if(nextProps.number %3 == 0){
+            return true 
+        }else{
+            return false 
+        }
+    }
+    componentWillUpdate(){
+        console.log('组件即将更新');
+    }
+    componentWillUnmount(){
+        console.log('组件要卸载掉')
+    }
+    componentDidUpdate(){
+        console.log('组件已更新');
+    }
+    render(){
+        console.log('组件重新渲染.....')
+        return(
+            <div>
+                {this.props.number}
+            </div>
+        )
+    }
+}
