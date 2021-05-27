d019ffdda875a98b0fbe5dbe487e5741fc3e4324
wujueying <652178160@qq.com>
2021-05-27 16:11:55 +0800

    git command

--- react_blog/service/app/controller/home.js
+++ react_blog/service/app/controller/home.js
@@ -7,6 +7,14 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
+  async list(){
+    const {ctx} = this;
+    ctx.body = '<h1>jspang blog list</h1>'
+  }
 }
 
 module.exports = HomeController;
+
+// RESTful  APP 前后端分离，简单 约束性
+// 请求方式 get  获取  post 新建 put 更新资源 delete 
+
