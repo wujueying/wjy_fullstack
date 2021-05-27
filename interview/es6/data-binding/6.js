b42c4e2400c8de03c1d90e2a16807dad28e8ea99
wujueying <652178160@qq.com>
2021-05-27 11:41:55 +0800

    git command

--- interview/es6/data-binding/6.js
+++ interview/es6/data-binding/6.js
@@ -1,16 +1,17 @@
 let target = {
-    _bar: 'foo',
-    _prop: 'bar',
-    prop: 'baz'
-};
+    _bar: 'foo', 
+    _prop: 'bar',  
+    prop: 'baz' 
+};   
 
-let handler = { 
+let handler = {   
     ownKeys (target) {  
-      return Reflect.ownKeys(target).filter(key => key[0] !== '_');
-    }
-};
-
-let proxy = new Proxy(target, handler);
-for (let key of Object.keys(proxy)) {
-  console.log(target[key]);
-} // baz 
+      return Reflect.ownKeys(target).filter(key => key[0] !== '_');  
+    }      
+};   
+     
+let proxy = new Proxy(target, handler);    
+for (let key of Object.keys(proxy)) {  
+  console.log(target[key]);  
+} 
+// baz 
