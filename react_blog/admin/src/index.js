d15bd4e93c52aa39dad810d5d99c214e1624ea2f
wujueying <652178160@qq.com>
2021-05-31 15:53:03 +0800

    git command

--- react_blog/admin/src/index.js
+++ react_blog/admin/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Main from './pages/Main';
 
 ReactDOM.render(
-   <App />,
+   <Main />,
   document.getElementById('root')
 );
 
