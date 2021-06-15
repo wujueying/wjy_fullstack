16560d7f10a5ee0cfcbe899e8c828f719b97fc4a
wujueying <652178160@qq.com>
2021-06-15 21:41:27 +0800

    git command

---
+++ vue/shop-cart/src/ProductList.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+      <li
+        v-for="product in products" 
+        :key="product.id"
+      >
+        {{product.title}} = {{product.price}}
+          <br/>
+          <button
+            @click="addProductToCart(product)"
+          >
+               Add to Cart  
+          </button>
+      </li>
+  </ul>
+</template>
+
+<script>
+// 读状态 products connect 
+// 要有一个方法把product -> cart
+// connect 高阶组件 -> props 
+import { mapState, mapActions } from 'vuex';
+export default { 
+    // 计算属性 
+   computed: mapState({
+       products: state => state.products.all
+   }),
+    methods: mapActions('cart', [
+        'addProductToCart'
+    ]),
+    created(){
+        this.$store.dispatch('products/getAllProducts');
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/shop-cart/src/store/modules/cart.js
@@ -0,0 +1,66 @@
+// 状态分支
+const state = () => ({
+    items: []
+});
+// 基于状态 ，返回对状态的新需求的getter属性
+// 丰富了状态 的读操作
+const getters = {
+    cartProducts: (state, getters, rootState) => {
+        return state.items.map(({id, quantity}) => {
+            const product = rootState.products.all.find(
+                product=> product.id == id)
+            return {
+                title: product.title,
+                price: product.price,
+                quantity
+            }
+        })
+    },
+    cartTotalPrice: (state, getters) => {
+        return getters.cartProducts.reduce((total, product) => {
+            return total + product.price * product.quantity
+        }, 0)
+    }
+}
+// commit 到mutation
+const actions = {
+    addProductToCart({ commit, state }, product) {
+        // commit('pushProductToCart', { id: product.id })
+        // 如果有库存
+        if(product.inventory > 0){
+            // items 
+            // 之前 买过吗？
+            const cartItem = state.items.find(
+                item => item.id == product.id , 
+            )
+            if(!cartItem){
+                commit('pushProductToCart', { id: product.id })
+            }else{
+                commit('incrementItemQuantity', cartItem)
+            }
+            // product.products.all  inventory
+        }
+    },
+    
+
+}
+// 只有mutation对状态进行写操作
+const mutations = {
+    pushProductToCart(state, { id }) {
+        state.items.push({
+            id,
+            quantity: 1
+        })
+    },
+    incrementItemQuantity(state, {id}){
+        const cartItem = state.items.find(item => item.id == id)
+        cartItem.quantity++
+    }
+}
+export default {
+    namespaced: true,
+    state, 
+    getters,
+    actions,
+    mutations
+}
