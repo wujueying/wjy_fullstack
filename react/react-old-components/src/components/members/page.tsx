f220f6eb70c4c77c51fc9d874d8d9ddd92d53a14
wujueying <652178160@qq.com>
2021-10-20 09:11:18 +0800

    补充

--- react/react-old-components/src/components/members/page.tsx
+++ react/react-old-components/src/components/members/page.tsx
@@ -1,37 +1,58 @@
 import * as React from 'react';
-import {memberAPI} from '../../api/member';
-// <> 泛型 泛指内部的关键数据类型
+import { memberAPI } from '../../api/member';
+//< > 泛型 泛指内部的关键数据类型
 interface Props {
 
 }
 interface State {
-    members: []
+  members: []
 }
 export class MembersPage extends React.Component<Props, State> {
-    constructor(props){
-        super(props);
-        this.state = {
-            members: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      members:[]
     }
-    // 请求
-    public componentDidMount(){
-        // 职责分离
-        memberAPI.fetchMembers()
-          .then(members => {
-              console.log(members);
-          })
-    }
-    render(){
-        return (
-            <div className="row">
-                <h2>Member Page</h2>
-                <table className="table">
-                    <tbody>
-
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+  }
+  // 请求
+  public componentDidMount() {
+    // 职责分离
+    memberAPI.fetchMembers()
+      .then((members) => {
+        console.log(members)
+        this.setState({
+          members:members
+        })
+      })
+  }
+  render() {
+    return (
+      <div className="row">
+        <h2>Member Page</h2>
+        <table className="table">
+          <tbody>
+          {
+            this.state.members.map(MemberRow)
+          }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+const MemberRow = (member) => {
+  return (
+    <tr key={member.id}>
+      <td>
+        <img src={member.avatar_url}
+         className="avatar"alt="" />
+      </td>
+      <td>
+        <span>{member.id}</span>
+      </td>
+      <td>
+        <span>{member.login}</span>
+      </td>
+    </tr>
+  )
 }
